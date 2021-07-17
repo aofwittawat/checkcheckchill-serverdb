832a1bd8514f98f457ba1f4050adf7facf2aae48
Wittawat Boonyanuwat <aofwittawat@gmail.com>
2021-07-18 00:22:43 +0700

    2nd

---
+++ models/ResultMale.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose')
+
+const ResultMaleSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    risk: [
+        {
+            age:{
+                type: Number,
+                required:true
+            },
+            weight:{
+                type: Number,
+                required:true
+            },
+            height:{
+                type: Number,
+                required:true
+            },
+            bmi:{
+                type: Number,
+                required:true
+            },
+            isSmoking: {
+                type: Boolean,
+                default: false
+            },
+            isDrinking: {
+                type: Boolean,
+                default: false
+            },
+            isVacineHepB: {
+                type: Boolean,
+                default: false
+            },
+            isExposure: {
+                type: Boolean,
+                default: false
+            },
+            isRenalFailure: {
+                type: Boolean,
+                default: false
+            },
+            isDM: {
+                type: Boolean,
+                default: false
+            },
+            isHT: {
+                type: Boolean,
+                default: false
+            },
+            isDLP: {
+                type: Boolean,
+                default: false
+            },
+            isHepatitis: {
+                type: Boolean,
+                default: false
+            },
+            isPancreaitis: {
+                type: Boolean,
+                default: false
+            },
+            isColitis: {
+                type: Boolean,
+                default: false
+            },
+            isOsteoporosis: {
+                type: Boolean,
+                default: false
+            },
+            isAsthma: {
+                type: Boolean,
+                default: false
+            },
+            isStone: {
+                type: Boolean,
+                default: false
+            },
+            isMI: {
+                type: Boolean,
+                default: false
+            },
+            isCAColon: {
+                type: Boolean,
+                default: false
+            },
+            isCAProstate: {
+                type: Boolean,
+                default: false
+            },
+            isCALiver: {
+                type: Boolean,
+                default: false
+            },
+            isCAPancreas: {
+                type: Boolean,
+                default: false
+            },
+            isCAOthers: {
+                type: Boolean,
+                default: false
+            },
+            
+
+        }
+    ]
+})
+
+module.exports = ResultMale = mongoose.model('resultMale', ResultMaleSchema)
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB
 
 app.get('/', (req,res) => res.send('API Running'))
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/resultmale', require('./routes/api/resultmale'))
+app.use('/api/resultfemale', require('./routes/api/resultmale'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log(`Server start on port ${PORT}`))
